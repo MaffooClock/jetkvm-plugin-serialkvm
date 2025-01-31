aebd60d00ad7c41f380745d5cdd7547718c5e818
Matthew Clark <matt@mclark.me>
2025-01-31 12:34:59 -0600

    Fix GoLand and .editorconfig fighting over indent

--- config.go
+++ config.go
@@ -1,72 +1,72 @@
 package main
 
 import (
-  "encoding/json"
-  "fmt"
-  "go.bug.st/serial"
-  "log"
-  "os"
+	"encoding/json"
+	"fmt"
+	"go.bug.st/serial"
+	"log"
+	"os"
 )
 
 type KvmInput struct {
-  ControlMessage string `json:"vendor_id"`
+	ControlMessage string `json:"vendor_id"`
 }
 
 type Config struct {
-  Device     string      `json:"device"`
-  SerialMode serial.Mode `json:"serial_mode"`
-  Inputs     []KvmInput  `json:"inputs"`
+	Device     string      `json:"device"`
+	SerialMode serial.Mode `json:"serial_mode"`
+	Inputs     []KvmInput  `json:"inputs"`
 }
 
 const configPath = "/userdata/jetkvm/plugins/serialkvm/config.json"
 
 var defaultConfig = &Config{
-  Device: "/dev/ttyS3",
-  SerialMode: serial.Mode{
-    BaudRate: 115200,
-    DataBits: 8,
-    Parity:   serial.NoParity,
-    StopBits: serial.OneStopBit,
-  },
+	Device: "/dev/ttyS3",
+	SerialMode: serial.Mode{
+		BaudRate: 115200,
+		DataBits: 8,
+		Parity:   serial.NoParity,
+		StopBits: serial.OneStopBit,
+	},
 }
 
 var config *Config
 
 func LoadConfig() {
-  if config != nil {
-    return
-  }
+	if config != nil {
+		return
+	}
 
-  file, err := os.Open(configPath)
-  if err != nil {
-    log.Println("default config file doesn't exist, using default")
-    config = defaultConfig
-    return
-  }
-  defer file.Close()
+	file, err := os.Open(configPath)
+	if err != nil {
+		log.Println("default config file doesn't exist, using default")
+		config = defaultConfig
+		return
+	}
+	defer file.Close()
 
-  var loadedConfig Config
-  if err := json.NewDecoder(file).Decode(&loadedConfig); err != nil {
-    log.Printf("config file JSON parsing failed, %v\n", err)
-    config = defaultConfig
-    return
-  }
+	var loadedConfig Config
+	if err := json.NewDecoder(file).Decode(&loadedConfig); err != nil {
+		log.Printf("config file JSON parsing failed, %v\n", err)
+		config = defaultConfig
+		return
+	}
 
-  config = &loadedConfig
+	config = &loadedConfig
 }
 
 func SaveConfig() error {
-  file, err := os.Create(configPath)
-  if err != nil {
-    return fmt.Errorf("failed to create config file: %w", err)
-  }
-  defer file.Close()
+	file, err := os.Create(configPath)
+	if err != nil {
+		return fmt.Errorf("failed to create config file: %w", err)
+	}
+	defer file.Close()
 
-  encoder := json.NewEncoder(file)
-  encoder.SetIndent("", "  ")
-  if err := encoder.Encode(config); err != nil {
-    return fmt.Errorf("failed to encode config: %w", err)
-  }
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(config); err != nil {
+		return fmt.Errorf("failed to encode config: %w", err)
+	}
 
-  return nil
+	return nil
 }
--- serialkvm.go
+++ serialkvm.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-  "fmt"
-  "log"
+	"fmt"
+	"log"
 
-  "go.bug.st/serial"
+	"go.bug.st/serial"
 )
 
 func (p *PluginImpl) NewSerialPort() error {
-  if p.serialPort != nil {
-    return nil
-  }
+	if p.serialPort != nil {
+		return nil
+	}
 
-  LoadConfig()
+	LoadConfig()
 
-  port, err := serial.Open(config.Device, &config.SerialMode)
-  if err != nil {
-    return fmt.Errorf("failed to open serial port %s: %w", config.Device, err)
-  }
-  defer port.Close()
+	port, err := serial.Open(config.Device, &config.SerialMode)
+	if err != nil {
+		return fmt.Errorf("failed to open serial port %s: %w", config.Device, err)
+	}
+	defer port.Close()
 
-  p.serialPort = port
+	p.serialPort = port
 
-  log.Printf("Opened port %s at %d baud\n", config.Device, config.SerialMode.BaudRate)
+	log.Printf("Opened port %s at %d baud\n", config.Device, config.SerialMode.BaudRate)
 
-  return nil
+	return nil
 }
 
 func (p *PluginImpl) SwitchInput(inputNumber int) error {
 
-  if inputNumber > len(config.Inputs) {
-    return fmt.Errorf("invalid input number: %d", inputNumber)
-  }
+	if inputNumber > len(config.Inputs) {
+		return fmt.Errorf("invalid input number: %d", inputNumber)
+	}
 
-  command := config.Inputs[inputNumber].ControlMessage
-  _, err := p.serialPort.Write([]byte(command))
-  if err != nil {
-    return fmt.Errorf("failed to send command: %w", err)
-  }
+	command := config.Inputs[inputNumber].ControlMessage
+	_, err := p.serialPort.Write([]byte(command))
+	if err != nil {
+		return fmt.Errorf("failed to send command: %w", err)
+	}
 
-  log.Printf("Switched to input %d\n", inputNumber)
+	log.Printf("Switched to input %d\n", inputNumber)
 
-  return nil
+	return nil
 }
